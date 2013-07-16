0b63e54c8961bbe39e8e350c6c1a3fc9ed0a4226
Vik Paruchuri <vik.paruchuri@gmail.com>
2013-07-15 21:22:09 -0400

    Initial

---
+++ config/settings.py
@@ -0,0 +1,40 @@
+"""
+Settings for simpsons-matcher
+"""
+
+from path import path
+import os
+import sys
+
+#Various paths
+PROJECT_PATH = path(__file__).dirname().dirname()
+
+#Where to cache values during the run
+CACHE = "percept.fields.caches.MemoryCache"
+#Do we use json to serialize the values in in the cache?
+SERIALIZE_CACHE_VALUES = False
+
+#How to run the workflows
+RUNNER = "percept.workflows.runners.SingleThreadedRunner"
+
+#What to use as a datastore
+DATASTORE = "percept.workflows.datastores.FileStore"
+
+#Namespace to give the modules in the registry
+NAMESPACE = "simpsons-matcher"
+
+#What severity of error to log to file and console.  One of "DEBUG", "WARN", "INFO", "ERROR"
+LOG_LEVEL = "DEBUG"
+
+#Used to save and retrieve workflows and other data
+DATA_PATH = os.path.abspath(os.path.join(PROJECT_PATH, "stored_data"))
+if not os.path.exists(DATA_PATH):
+    os.makedirs(DATA_PATH)
+
+#Commands are discovered here, and tasks/inputs/formats are imported using only these modules
+INSTALLED_APPS = [
+    'simpsons-matcher.inputs',
+    'simpsons-matcher.formatters',
+    'simpsons-matcher.tasks',
+    'simpsons-matcher.workflows'
+]
