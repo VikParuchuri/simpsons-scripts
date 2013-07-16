3654826239340c98640fdb27f1c47b842698e1a5
Vik Paruchuri <vik.paruchuri@gmail.com>
2013-07-16 13:58:09 -0400

    Transcript crawler

--- crawler/crawler/spiders/scrape.py
+++ crawler/crawler/spiders/scrape.py
@@ -5,7 +5,8 @@ from scrapy.selector import HtmlXPathSelector
 from scrapy.item import Item, Field
 import re
 
-base_url = "http://www.snpp.com/episodes.html"
+snpp_base_url = "http://www.snpp.com/episodes.html"
+ss_base_url = "http://www.springfieldspringfield.co.uk/episode_scripts.php"
 
 class Script(Item):
     url = Field()
@@ -15,7 +16,7 @@ class Script(Item):
 class SimpsonsSpider(CrawlSpider):
     name = "snpp"
     allowed_domains = ['www.snpp.com', 'snpp.com']
-    start_urls = [base_url]
+    start_urls = [snpp_base_url]
     rules = [Rule(SgmlLinkExtractor(allow=['/episodes/\w+.html']), 'parse_script')]
 
     def fix_field_names(self, field_name):
@@ -31,4 +32,25 @@ class SimpsonsSpider(CrawlSpider):
         script['url'] = response.url
         script['episode_name'] = "".join(x.select("//h1/text()").extract())
         script['script'] = " ".join(x.select("//pre/text()").extract())
+        return script
+
+class SubtitleSpider(CrawlSpider):
+    name = "ss"
+    allowed_domains = ['www.springfieldspringfield.co.uk', 'springfieldspringfield.co.uk']
+    start_urls = [ss_base_url]
+    rules = [Rule(SgmlLinkExtractor(allow=['/view_episode_scripts.php\?tv-show=the-simpsons&episode=\w+']), 'parse_script')]
+
+    def fix_field_names(self, field_name):
+        field_name = re.sub(" ","_", field_name)
+        field_name = re.sub(":","", field_name)
+        return field_name
+
+    def parse_script(self, response):
+        x = HtmlXPathSelector(response)
+
+        script = Script()
+
+        script['url'] = response.url
+        script['episode_name'] = "".join(x.select("//h3/text()").extract())
+        script['script'] = " ".join(x.select("//div[@class='episode_script']/text()").extract())
         return script
