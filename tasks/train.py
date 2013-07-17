0de8583f14b8dcdacdfa6f1db8a300562c27124f
Vik Paruchuri <vik.paruchuri@gmail.com>
2013-07-16 22:39:33 -0400

    Increase max features, remove counter stop

--- tasks/train.py
+++ tasks/train.py
@@ -22,7 +22,7 @@ import random
 import logging
 log = logging.getLogger(__name__)
 
-MAX_FEATURES = 150
+MAX_FEATURES = 250
 DISTANCE_MIN=1
 RESET_SCENE_EVERY = 1000
 
@@ -318,8 +318,6 @@ class KNNRF(Task):
         for script in test_data['voice_script']:
             counter+=1
             log.info("On script {0} out of {1}".format(counter,len(test_data['voice_script'])))
-            if counter>5:
-                break
             lines = script.split("\n")
             speaker_code = [-1 for i in xrange(0,len(lines))]
             for (i,line) in enumerate(lines):
