2b3900feaf038dc8b15e6afa166fe779607db2e1
Vik Paruchuri <vik.paruchuri@gmail.com>
2013-07-19 08:06:07 -0400

    Use multiprocessing

--- tasks/audio.py
+++ tasks/audio.py
@@ -14,6 +14,7 @@ import numpy as np
 from scikits.audiolab import Sndfile
 from scikits.audiolab import oggread
 import pandas as pd
+from multiprocessing import Pool
 
 log = logging.getLogger(__name__)
 
@@ -53,70 +54,6 @@ class LoadAudioFiles(Task):
 
         return None, None
 
-    def calc_slope(self,x,y):
-        x_mean = sum(x)/len(x)
-        y_mean = sum(y)/len(y)
-        x_dev = sum([abs(i-x_mean) for i in x])
-        y_dev = sum([abs(i-y_mean) for i in y])
-
-        slope = (x_dev*y_dev)/(x_dev*x_dev)
-        return slope
-
-    def get_indicators(self,vec):
-        mean = sum(vec)
-        slope = self.calc_slope(list(range(len(vec))),vec)
-        std = np.std(vec)
-        return mean, slope, std
-
-    def calc_u(self,vec):
-        fft = np.fft.fft(vec)
-        return np.sum(np.multiply(fft,vec))/np.sum(vec)
-
-    def calc_features(self, vec,freq):
-        #bin count
-        bc = 10
-        bincount = list(range(bc))
-        #framesize
-        fsize = 512
-        #mean
-        m = np.mean(vec)
-        #spectral flux
-        sf = np.mean(vec-np.roll(vec,fsize))
-        mx = np.max(vec)
-        mi = np.min(vec)
-        sdev = np.std(vec)
-        binwidth = len(vec)/bc
-        bins = []
-        for i in xrange(0,bc):
-            bins.append(vec[(i*binwidth):(binwidth*i + binwidth)])
-        peaks = [np.max(i) for i in bins]
-        mins = [np.min(i) for i in bins]
-        amin,smin,stmin = self.get_indicators(mins)
-        apeak, speak, stpeak = self.get_indicators(peaks)
-        cepstrums = [np.fft.ifft(np.log(np.abs(np.fft.fft(i)))) for i in bins]
-        inter = [self.get_indicators(i) for i in cepstrums]
-        acep,scep, stcep = self.get_indicators([i[0] for i in inter])
-        aacep,sscep, stsscep = self.get_indicators([i[1] for i in inter])
-
-        zero_crossings = np.where(np.diff(np.sign(vec)))[0]
-        zcc = len(zero_crossings)
-        zccn = zcc/freq
-
-        u = [self.calc_u(i) for i in bins]
-        spread = np.sqrt(u[-1] - u[0]**2)
-        skewness = (u[0]**3 - 3*u[0]*u[5] + u[-1])/spread**3
-
-        #Spectral slope
-        ss = self.calc_slope(range(len(vec)),np.fft.fft(vec))
-        avss = self.calc_slope(bincount,[self.calc_slope(range(len(i)),np.fft.fft(i)) for i in bins])
-
-        return [m,sf,mx,mi,sdev,amin,smin,stmin,apeak,speak,stpeak,acep,scep,stcep,aacep,sscep,stsscep,zcc,zccn,spread,skewness,ss,avss]
-
-    def extract_features(self,sample,freq):
-        left = self.calc_features(sample[:,0],freq)
-        right = self.calc_features(sample[:,1],freq)
-        return left+right
-
     def predict(self, data, **kwargs):
         """
         Used in the predict phase, after training.  Override
@@ -141,27 +78,19 @@ class LoadAudioFiles(Task):
             if subtitle_frame.shape[0]==0:
                 continue
             print "On file {0}".format(counter)
-            counter+=1
-            if counter>1:
-                break
             f_data, fs, enc  = oggread(f)
-            current_frame = 0
             subtitle_frame = subtitle_frame.sort('start')
             subtitle_frame.index = range(subtitle_frame.shape[0])
-            audio_samples = []
-            audio_features = []
-            good_rows = []
+            samps = []
             for i in xrange(0,subtitle_frame.shape[0]):
                 start = subtitle_frame['start'].iloc[i]
                 end = subtitle_frame['end'].iloc[i]
                 samp = f_data[(start*fs):(end*fs),:]
-                try:
-                    features = self.extract_features(samp,fs)
-                    audio_features.append(features)
-                    good_rows.append(i)
-                except Exception:
-                    log.exception("Cannot generate features")
-                    continue
+                samps.append({'samp' : samp, 'fs' : fs})
+            p = Pool(4)
+            results = p.map(process_subtitle, samps)
+            good_rows = [i for i in xrange(0,len(results)) if results[i]!=None]
+            audio_features = [i for i in results if i!=None]
             df = pd.concat([subtitle_frame.iloc[good_rows],pd.DataFrame(audio_features)],axis=1)
             df = df.fillna(-1)
             df.index = range(df.shape[0])
@@ -170,3 +99,74 @@ class LoadAudioFiles(Task):
         data.index = range(data.shape[0])
 
         return data
+
+def calc_slope(x,y):
+    x_mean = sum(x)/len(x)
+    y_mean = sum(y)/len(y)
+    x_dev = sum([abs(i-x_mean) for i in x])
+    y_dev = sum([abs(i-y_mean) for i in y])
+
+    slope = (x_dev*y_dev)/(x_dev*x_dev)
+    return slope
+
+def get_indicators(vec):
+    mean = sum(vec)
+    slope = calc_slope(list(range(len(vec))),vec)
+    std = np.std(vec)
+    return mean, slope, std
+
+def calc_u(vec):
+    fft = np.fft.fft(vec)
+    return np.sum(np.multiply(fft,vec))/np.sum(vec)
+
+def calc_features(vec,freq):
+    #bin count
+    bc = 10
+    bincount = list(range(bc))
+    #framesize
+    fsize = 512
+    #mean
+    m = np.mean(vec)
+    #spectral flux
+    sf = np.mean(vec-np.roll(vec,fsize))
+    mx = np.max(vec)
+    mi = np.min(vec)
+    sdev = np.std(vec)
+    binwidth = len(vec)/bc
+    bins = []
+    for i in xrange(0,bc):
+        bins.append(vec[(i*binwidth):(binwidth*i + binwidth)])
+    peaks = [np.max(i) for i in bins]
+    mins = [np.min(i) for i in bins]
+    amin,smin,stmin = get_indicators(mins)
+    apeak, speak, stpeak = get_indicators(peaks)
+    cepstrums = [np.fft.ifft(np.log(np.abs(np.fft.fft(i)))) for i in bins]
+    inter = [get_indicators(i) for i in cepstrums]
+    acep,scep, stcep = get_indicators([i[0] for i in inter])
+    aacep,sscep, stsscep = get_indicators([i[1] for i in inter])
+
+    zero_crossings = np.where(np.diff(np.sign(vec)))[0]
+    zcc = len(zero_crossings)
+    zccn = zcc/freq
+
+    u = [calc_u(i) for i in bins]
+    spread = np.sqrt(u[-1] - u[0]**2)
+    skewness = (u[0]**3 - 3*u[0]*u[5] + u[-1])/spread**3
+
+    return [m,sf,mx,mi,sdev,amin,smin,stmin,apeak,speak,stpeak,acep,scep,stcep,aacep,sscep,stsscep,zcc,zccn,spread,skewness]
+
+def extract_features(sample,freq):
+    left = calc_features(sample[:,0],freq)
+    right = calc_features(sample[:,1],freq)
+    return left+right
+
+def process_subtitle(d):
+    samp = d['samp']
+    fs = d['fs']
+    try:
+        features = extract_features(samp,fs)
+    except Exception:
+        log.exception("Cannot generate features")
+        return None
+
+    return features
